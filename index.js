83f4d645567e677cecb5e32d8bf5f400cfae7a21
Gilles Coomans <gilles.coomans@gmail.com>
2016-02-13 01:19:40 +0100

    follow exec and dom isolation refactorings in index

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 /**  @author Gilles Coomans <gilles.coomans@gmail.com> */
-// core
 var y = function(t) {
 	return new y.Template(t);
 };
 
+// yamvish core
+y.api = require('./lib/api');
 y.env = require('./lib/env');
 y.utils = require('./lib/utils');
-y.Context = require('./lib/context');
-y.Template = require('./lib/template');
-y.PureNode = require('./lib/pure-node');
-y.Container = require('./lib/container');
-y.Filter = require('./lib/filter');
 y.AsyncManager = require('./lib/async');
+y.Context = require('./lib/context');
 var interpolable = require('./lib/interpolable');
 y.interpolable = interpolable.interpolable;
 y.Interpolable = interpolable.Interpolable;
-y.addCustomTag = require('./lib/custom-tags');
-y.listenerParser = require('./lib/parsers/listener-call');
-y.elenpi = require('elenpi');
-y.api = require('./lib/api');
-require('./lib/output-engine/dom');
+y.Filter = require('./lib/filter');
 
+// Templates
+y.Template = require('./lib/template');
 y.View = require('./lib/view');
 y.view = function(data, parent, path) {
 	return new y.View(data, parent, path);
 };
-y.html = require('./lib/parsers/html-to-template');
 
-
+// API management
+y.addCustomTag = require('./lib/custom-tags');
 y.toAPI = function(apiName, methodsObj) {
 	var api = y.api[apiName] = y.api[apiName] || {};
 	y.utils.shallowMerge(methodsObj, api);
 	return api;
 };
 
+// PARSERS and related
+y.elenpi = require('elenpi');
+y.listenerParser = require('./lib/parsers/listener-call');
+y.html = require('./lib/parsers/html-to-template');
+
+// DOM engine
+// y.PureNode = require('./lib/pure-node');
+y.Container = require('./lib/output-engine/dom/container');
+require('./lib/output-engine/dom/engine');
+
 module.exports = y;
-
-
-/*
-	Polyfills for IE9: 
-
-	es6-promise or promis
-	history API if router 
- */
