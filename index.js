305f8121d99cbbc233c687fba9d02d28ec226014
Gilles Coomans <gilles.coomans@gmail.com>
2016-02-22 22:24:37 +0100

    clean index

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ y.env = require('./lib/env');
 y.utils = require('./lib/utils');
 y.AsyncManager = require('./lib/async');
 y.Context = require('./lib/context');
+y.Filter = require('./lib/filter');
 var interpolable = require('./lib/interpolable');
 y.interpolable = interpolable.interpolable;
 y.Interpolable = interpolable.Interpolable;
-y.Filter = require('./lib/filter');
 
 // Templates
 y.Template = require('./lib/template');
@@ -35,7 +35,6 @@ y.listenerParser = require('./lib/parsers/listener-call');
 y.html = require('./lib/parsers/html-to-template');
 
 // DOM engine
-// y.PureNode = require('./lib/pure-node');
 y.Container = require('./lib/output-engine/dom/container');
 require('./lib/output-engine/dom/engine');
 
