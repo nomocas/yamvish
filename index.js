d9854c25d65f48f927a55761ab36454db3bd4706
Gilles Coomans <gilles.coomans@gmail.com>
2016-11-04 11:41:59 +0100

    lot of things to follow babelutes. should be backward compliant.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 /**  @author Gilles Coomans <gilles.coomans@gmail.com> */
-var y = function(t) {
-	return new y.Template(t);
-};
+var api = require('./lib/api'),
+	y = function(apiName) {
+		if (apiName)
+			return api.initializers[apiName];
+		return new y.Template();
+	};
 
 // yamvish core
-y.api = require('./lib/api');
 y.env = require('./lib/env');
 y.utils = require('./lib/utils');
 
@@ -23,31 +25,33 @@ y.Interpolable = interpolable.Interpolable;
 y.Template = require('./lib/template');
 
 // API management
-y.toAPI = function(apiName, methodsObj) {
-	var api = y.api[apiName] = y.api[apiName] || {};
-	y.utils.shallowMerge(methodsObj, api);
-	return api;
+y.toAPI = function(name, methods) {
+	return api.toAPI(name, methods);
 };
+y.extendAPI = function(base, newName, methods) {
+	return api.extendAPI(base, newName, methods);
+};
+y.initializer = function(name) {
+	return api.initializer(name);
+};
+y.init = api.initializers.yamvish;
 
 // PARSERS and related
 y.elenpi = require('elenpi');
 y.listenerParser = require('./lib/parsers/listener-call');
 y.html = require('./lib/parsers/html-to-template');
-y.emmet = require('./lib/parsers/emmet-style');
+// y.emmet = require('./lib/parsers/emmet-style');
 
 // load DOM engine by default
 require('./lib/output-engine/dom/engine');
 y.Container = require('./lib/output-engine/dom/container');
 
-y.Error = function(status, message, report) {
-	this.status = status;
-	this.message = message;
-	this.report = report;
-}
-y.Error.prototype = new Error();
+y.Error = require('nomocas-utils/lib/error')
 
 var plateform = require('nomocas-webutils/lib/plateform');
 for (var i in plateform)
 	y[i] = plateform[i];
+
+
 
 module.exports = y;
